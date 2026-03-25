2386cf315fc52f6b9d2363ea98a7b1836307f007
Franco Matias Rossi <rossifrancomatias@gmail.com>
2026-03-25 01:41:19 -0300

    feat: add initial Strapi middleware configuration, including CORS setup.

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,17 +1,17 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::poweredBy',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['*'],
+      origin: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
     },
   },
+  'strapi::security',
+  'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
